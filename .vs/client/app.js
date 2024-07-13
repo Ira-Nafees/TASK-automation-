095f22c9cc81e07458ae58e85ba84e1e94bc858a
ira nafees <ira.nafees15@gmail.com>
2024-07-14 00:22:30 +0530

    first commit

---
+++ .vs/client/app.js
@@ -0,0 +1,7 @@
+//console.log('Hello');
+//const heading=document.createElement('h2');
+//heading.innerText = "Hello pp";
+import React from "react"= React.createElement('h2',{},"Hello pp ");
+const domroot=document.getElementById('root');
+const reactRoot = ReactDom.createRoot(domRoot);
+reactRoot.render(head)
